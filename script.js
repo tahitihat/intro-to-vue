453384763d577c17731bf1f2201912c6843783f7
Isabel Tripp <stagestruck1@gmail.com>
2020-11-14 21:52:49 -1000

    lesson 7 - computed properties

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'NASA socks',
-        image: './assets/jwt-Socks.jpg',
+        brand: 'NASA',
+        product: 'Socks',
+        selectedVariant: 0,
         url: "https://www.etsy.com/listing/755853484/james-webb-space-telescope-jwst-science?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=astronomy+socks&ref=sr_gallery-1-22&frs=1",
-        inStock: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantName: 'James Webb Telescope',
                 variantColor: 'blue',
-                variantImage: './assets/jwt-Socks.jpg'
+                variantImage: './assets/jwt-Socks.jpg',
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantName: 'Space Shuttle',
                 variantColor: 'green',
-                variantImage: './assets/ss-Socks.jpg'
+                variantImage: './assets/ss-Socks.jpg',
+                variantQuantity: 0
             }
         ],
         cart: 1
@@ -26,8 +28,19 @@ var app = new Vue({
         addToCart() {
             this.cart += 1;
         },
-        updateProduct(img) {
-            this.image = img;
+        updateProduct(idx) {
+            this.selectedVariant = idx;
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + " " + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() { 
+            return this.variants[this.selectedVariant].variantQuantity > 0
         }
     }
 });
