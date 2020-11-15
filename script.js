53310574dee24a7ea0a9a224bb63298906a49085
Isabel Tripp <stagestruck1@gmail.com>
2020-11-14 20:42:31 -1000

    lesson 2 - attirbute binding

---
+++ script.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: './assets/jwt-Socks.jpg',
+        url: "https://www.etsy.com/listing/755853484/james-webb-space-telescope-jwst-science?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=astronomy+socks&ref=sr_gallery-1-22&frs=1"
+    }
+});
