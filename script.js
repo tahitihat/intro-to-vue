6b39879fc10733566dff07a40d53a55636f1bd98
Isabel Tripp <stagestruck1@gmail.com>
2020-11-14 21:44:12 -1000

    lesson 6 - class binding

--- script.js
+++ script.js
@@ -4,17 +4,19 @@ var app = new Vue({
         product: 'NASA socks',
         image: './assets/jwt-Socks.jpg',
         url: "https://www.etsy.com/listing/755853484/james-webb-space-telescope-jwst-science?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=astronomy+socks&ref=sr_gallery-1-22&frs=1",
-        inventory: 100,
+        inStock: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'James Webb Telescope',
+                variantName: 'James Webb Telescope',
+                variantColor: 'blue',
                 variantImage: './assets/jwt-Socks.jpg'
             },
             {
                 variantId: 2235,
-                variantColor: 'Space Shuttle',
+                variantName: 'Space Shuttle',
+                variantColor: 'green',
                 variantImage: './assets/ss-Socks.jpg'
             }
         ],
